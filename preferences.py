ee58f030d5c25f854ea5c242e0e8511aa8c48c44
klaas8 <132535742+klaas8@users.noreply.github.com>
2026-01-01 13:11:42 +0800

    创建 preferences.py

---
+++ preferences.py
@@ -0,0 +1,30 @@
+from sqlitedict import SqliteDict
+
+class AppPreferences:
+    _instance = None
+    
+    def __new__(cls, db_path="app_prefs.sqlite"):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.db = SqliteDict(db_path, autocommit=True)
+        return cls._instance
+    
+    def get(self, key, default=None):
+        return self.db.get(key, default)
+    
+    def put(self, key, value):
+        self.db[key] = value
+        self.db.commit()
+    
+    def remove(self, key):
+        if key in self.db:
+            del self.db[key]
+            self.db.commit()
+    
+    def clear(self):
+        self.db.clear()
+        self.db.commit()
+    
+    def close(self):
+        self.db.close()
+        self._instance = None
